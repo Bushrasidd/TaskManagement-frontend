4dbdd4860063e162542b73643dcfbdabaefaa7c7
Bushra <aayatkhabam391@gmail.com>
2026-02-23 23:08:51 +0530

    Add task status cards to App component layout; update main.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,35 @@ function App() {
         {/* <button className='btn btn-light ms-5'>Create</button> */}
         </div>
         <div className='d-flex justify-content-center align-items-center mt-4'>
-          {/*if i dont give the justify conetnt center and align item center why is search bar taking whole width */}
           <SearchBar onSearch={handleSearchAction} />
+        </div>
+        <div className='container-fluid p-4 mt-4'>
+          <div className="row">
+            <div className="col-12 col-lg-3">
+              <div className="card border-warning p-3" style={{ minHeight: "400px"}}>
+            <h5 className="text-warning">PENDING</h5>
+            {/* <div className="border p-2 mt-2">Pending</div> */}
+          </div>
+        </div>
+          <div className="col-12 col-lg-3">
+              <div className="card border-warning p-3" style={{ minHeight: "400px" }}>
+            <h5 className="text-warning">IN PROGRESS</h5>
+            {/* <div className="border p-2 mt-2">In-progress</div> */}
+          </div>
+        </div>
+          <div className="col-12 col-lg-3">
+              <div className="card border-warning p-3" style={{ minHeight: "400px" }}>
+            <h5 className="text-warning">COMPLETED</h5>
+            {/* <div className="border p-2 mt-2">Completed</div> */}
+          </div>
+        </div>
+          <div className="col-12 col-lg-3">
+              <div className="card border-warning p-3" style={{ minHeight: "400px" }}>
+            <h5 className="text-warning">REVIEW</h5>
+            {/* <div className="border p-2 mt-2">Review</div> */}
+          </div>
+        </div>
+        </div>
         </div>
         </main> 
         </div> 
@@ -28,3 +55,4 @@ function App() {
 }
 
 export default App
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'primereact/resources/themes/lara-light-blue/theme.css'
-import 'primereact/resources/primereact.min.css'
+// import 'primereact/resources/themes/lara-light-blue/theme.css'
+// import 'primereact/resources/primereact.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
